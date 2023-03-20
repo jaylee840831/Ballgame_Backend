615a4d170b375a02fa5620bf9b386f9505447f66
jasper0831 <lyinjue456852@gmail.com>
2023-03-20 11:24:15 +0800

    上傳jar

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Ballgame_Backend
+
+## 啟動 Project
+
+    直接執行jar檔
+        1. 開啟命令提示元
+
+    
+方法二    
+
+## 開發工具
+
+1. Spring Boot
+2. Java 17
+
+## 資料庫schema
--- src/main/java/com/example/ballgame/service/JwtService.java
+++ src/main/java/com/example/ballgame/service/JwtService.java
@@ -61,7 +61,7 @@ import io.jsonwebtoken.security.Keys;
 public class JwtService {
 	
 	//密鑰
-	private static final String SECRET_KEY = "";
+	private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
 
 	public String extractUserEmail(String token) {
 		return extractClaim(token, Claims::getSubject);
